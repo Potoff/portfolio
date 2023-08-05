7f95695ce7b9b57deb175ed6e5792061badea18e
Potoff <services.ankaamusic@gmail.com>
2023-08-05 16:58:02 +0200

    responsive home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,14 +5,14 @@ import HomeSvg from '@/components/HomeSvg.vue';
 <template>
   <main class="h-full overflow-hidden">
     <div class="flex flex-col items-center justify-between content-between h-fit md:flex-row md:justify-center md:mt-36">
-      <div class="flex flex-col justify-between items-center content-between h-60">
-        <h1 class="text-6xl font-bold text-green-700">Paul Demarest</h1>
-        <div class="flex text-4xl font-bold text-gray-500">
+      <div class="flex flex-col justify-end md:justify-between items-center h-60">
+        <h1 class="text-4xl md:text-6xl font-bold text-green-700">Paul Demarest</h1>
+        <div class="flex text-1xl md:text-4xl font-bold text-gray-500 my-2 md:my-0">
           <p>Developpeur / Integrateur web</p>
         </div>
         <!-- make a button to projects -->
         <router-link to="/projects"
-          class="text-gray-100 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-xl font-medium border-rounded border bg-green-600 w-24">
+          class="text-gray-100 hover:bg-green-700 hover:text-white px-3 py-2 rounded-md text-lg md:text-xl font-medium border-rounded border bg-green-600 w-24">
           Projets
         </router-link>
       </div>
