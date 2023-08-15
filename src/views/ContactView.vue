4df31ca5e19926e33826153b2ffbdba5a9fcd49f
Potoff <services.ankaamusic@gmail.com>
2023-08-15 18:27:06 +0200

    add emailjs

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,34 +1,34 @@
 <script setup lang="ts">
 
-
 </script>
 
 <template>
-  <div class="w-full max-w-xs mx-auto">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <div class="w-full max-w-xs mx-auto my-20">
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" id="contact-form">
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-          Username
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+          Nom
         </label>
         <input
           class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="username" type="text" placeholder="Username">
+          id="name" type="text" placeholder="Jean" name="name">
       </div>
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-          Password
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="mail">
+          Mail
         </label>
         <input
           class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password" type="password" placeholder="******************">
+          id="mail" type="text" placeholder="jean@valjean.com" name="mail">
       </div>
       <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        <label class="block text-gray-700 text-sm font-bold mb-2" for="message">
           Message
         </label>
-        <textarea class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          id="password" type="" placeholder="votre message" size="40"></textarea>
-          
+        <textarea
+          class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          id="message" type="" placeholder="votre message" size="40" name="message"></textarea>
+
       </div>
       <div class="flex items-center justify-between">
         <button
@@ -38,6 +38,7 @@
         </button>
       </div>
     </form>
-</div></template>
+  </div>
+</template>
 
 <style></style>
